a7c4e4dc39a2768531372653c63511412cd9ac66
Shanwill Pinto <shanwillpinto@gmail.com>
2022-06-15 19:55:26 +0530

    led

--- Device Back-end/app.py
+++ Device Back-end/app.py
@@ -3,10 +3,16 @@ from flask import Flask, Response, jsonify
 from imutils.video import VideoStream
 import numpy as np
 import imutils
+import time
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 sub = cv2.createBackgroundSubtractorMOG2()  # create background subtractor
-
+pin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+pwm = GPIO.PWM(pin, 100)
+pwm.start(0)
 
 CLASSES = [ "background", "aeroplane", "bicycle", "bird", "boat",
 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
@@ -31,14 +37,17 @@ def hello_world():  # put application's code here
 
 def gen():
     count = []
-    vs = VideoStream(src=0).start()
+    # vs = VideoStream(src=2).start()
+
+    # Video from file using imutils
+    vs = cv2.VideoCapture(filename='Device Back-end\video.mp4')
 
     # loop over the frames from the video stream
     while True:
         # grab the frame from the threaded video stream and resize it
         # to have a maximum width of 400 pixels
-        image = vs.read()
-        image = imutils.resize(image, width=1080)
+        _,image = vs.read()
+        image = imutils.resize(image, width=500)
 
         # grab the frame dimensions and convert it to a blob
         (h, w) = image.shape[:2]
@@ -81,6 +90,23 @@ def gen():
             "count" : len(count),
             "items" : count
         })
+        morn = datetime.now()
+        eve = datetime.now()
+        morn = morn.replace(hour=6, minute=0, second=0, microsecond=0)
+        eve = eve.replace(hour=18, minute=0, second=0, microsecond=0)
+        time = datetime.now()
+        if (time > morn and time < eve):
+            print("Lights off")
+            GPIO.output(LED_PIN, GPIO.LOW)
+        else:
+            if(len(count)>=0 and len(count)<=2):
+                print("Intensity 20%");
+                # GPIO.output(LED_PIN, GPIO.HIGH)
+                pwm.ChangeDutyCycle(20)
+                
+            elif(len(count)>2):
+                print("Intensity 100%");
+                pwm.ChangeDutyCycle(100);
         count = []
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         # time.sleep(0.1)
